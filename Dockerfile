b941d30aef33da317a14c8d6cebb93cdd67514fe
Misha Marinenko <0x77dev@protonmail.com>
2019-12-10 00:38:21 +0200

    Transliteration for understanding russian, non trivial changes...

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
-FROM node:latest
+FROM node:13.3.0-buster
 RUN mkdir /app
+RUN apt update
+RUN apt install -y build-ess* pkg-config libxi-dev libglu1-mesa-dev libglew-dev
 COPY . /app/
 WORKDIR /app/
 RUN yarn install
+RUN chmod 777 /app/*
+RUN chmod 777 /app
 CMD ["node", "."]
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const Telegraf = require("telegraf");
 const Brain = new (require("./lib/Brain"))();
+const translit = require("./translit");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.on("message", (ctx) => {
 	let message = ctx.message.text;
+	message = translit(message);
 	let replyMessage = (ctx.message.reply_to_message !== undefined ? ctx.message.reply_to_message.text : false);
+	if (replyMessage !== false) replyMessage = translit(replyMessage);
 	console.log(replyMessage);
 	let out = Brain.run(message);
 	if (replyMessage !== false) { Brain.Memory.addTrainingData(replyMessage, message); Brain.train(); return ctx.replyWithHTML("<b>Обучился</b>"); }
 	else {
-		if (out !== "") return ctx.reply(out);
+		if (out !== "") return ctx.reply(translit(out, true), Telegraf.Extra.inReplyTo(ctx.message.reply_to_message.message_id));
 	}
 });
 
